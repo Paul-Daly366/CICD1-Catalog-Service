deaba51cfb11c81c8d0e83ed407fd99edaf4582a
PaulDaly-STUDENT <g00470372@atu.ie>
2026-09-18 09:32:59 +0100

    Initial Commit for Catalog Service

---
+++ src/main/java/com/example/cicd1_catalog_service/Cicd1CatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicd1_catalog_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cicd1CatalogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cicd1CatalogServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cicd1_catalog_service/Cicd1CatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicd1_catalog_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cicd1CatalogServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
