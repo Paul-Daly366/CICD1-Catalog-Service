fdcf4109aa32c32136f5302066109f68fb2400fb
PaulDaly-STUDENT <g00470372@atu.ie>
2026-09-24 14:15:21 +0100

    Product converted into an entity, ProductRepository created, ProductService refactored, all tested and working

--- src/main/java/com/example/cicd1_catalog_service/model/Product.java
+++ src/main/java/com/example/cicd1_catalog_service/model/Product.java
@@ -1,14 +1,22 @@
 package com.example.cicd1_catalog_service.model;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.math.BigDecimal;
 
+@Entity
+@Table(name = "products")
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private BigDecimal price;
+
+
 }
--- src/main/java/com/example/cicd1_catalog_service/service/ProductService.java
+++ src/main/java/com/example/cicd1_catalog_service/service/ProductService.java
@@ -1,6 +1,7 @@
 package com.example.cicd1_catalog_service.service;
 
 import com.example.cicd1_catalog_service.model.Product;
+import com.example.cicd1_catalog_service.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -8,17 +9,20 @@ import java.util.List;
 
 @Service
 public class ProductService {
-    private final List<Product> products = new ArrayList<>();
+    private final ProductRepository repository;
     private long nextId = 1;
 
-    public List<Product> getAll(){
-        return products;
+    public ProductService(ProductRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Product> getAll() {
+        return repository.findAll();
     }
 
     public Product create(Product product){
-        product.setId(nextId++);
-        products.add(product);
-        return product;
+        product.setId(null);
+        return repository.save(product);
     }
 
 }
